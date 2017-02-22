d0b624e13d0b8809d21c2f5553ce610d709aad6b
Richard Gan <richard@designedbymajor.ca>
2017-02-21 21:10:06 -0800

    minor changes to events
    spacing and making sure message event actually works

--- events/message.js
+++ events/message.js
@@ -2,8 +2,8 @@ const unflips = require("../lists/unflips.json");
 const tableflipRegex = /(?:ʕノ•ᴥ•ʔノ|\(\/¯◡ ‿ ◡\)\/¯ ~|\(._.\) ~|\(╯'□'\)╯|\(╯°□°）╯|\(ノ ゜Д゜\)ノ|\‎?\(ﾉಥ益ಥ）ﾉ\﻿?)\s?︵? ┻(━*)┻/;  //eslint-disable-line no-irregular-whitespace
 
 const tableUnflipper = message => {
-    let flippedTable = message.content.match(tableflipRegex);
-    if (flippedTable) {
+  let flippedTable = message.content.match(tableflipRegex);
+  if (flippedTable) {
     let chance = Math.round(Math.random() * 100);
     if (chance < 10) {
       return message.reply(unflips.gif[Math.floor(Math.random() * unflips.gif.length)]);
@@ -15,28 +15,28 @@ const tableUnflipper = message => {
 };
 
 module.exports = msg => {
-  const client = message.client;
-  
+  const client = msg.client;
+
   // restrictions
   if (msg.author.id === client.user.id) return;
   if (msg.author.bot) return;
-  
+
   let prefix;
-  if (msg.content.startsWith(client.config.prefix)) prefix = bot.config.prefix;
-  else if (msg.content.startsWith(`${client.user} `)) uPrefix = `${client.user} `;
+  if (msg.content.startsWith(client.config.prefix)) prefix = client.config.prefix;
+  else if (msg.content.startsWith(`${client.user} `)) prefix = `${client.user} `;
   else return tableUnflipper(msg);
-  
+
   // command interpreting
   let command = msg.content.substring(prefix.length).split(" ")[0];
   let suffix = msg.content.substring(prefix.length + command.length + 1);
   let permLevel = client.util.checkPermLevel(msg);
   let cmd = client.commands.has(command) ? client.commands.get(command) : client.commands.get(client.aliases.get(command));
-  
+
   if (cmd) {
     if (!cmd.config.enabled) return;
     if (cmd.config.guildOnly && !msg.guild) return;
     if (permLevel < cmd.config.permLevel) return;
-    
+
     let location = msg.guild ? `${msg.guild.name} (#${msg.channel.name})` : "private messages";
     cmd.run(client, msg, suffix);
     client.log(`Command run by ${msg.author.username}#${msg.author.discriminator} in ${location}: ${msg.content}`);
--- util/loadEvents.js
+++ util/loadEvents.js
@@ -6,6 +6,7 @@ module.exports = client => {
   client.on("reconnect", () => reqEvent("reconnect")(client));
   client.on("message", reqEvent("message"));
   client.on("voiceStateUpdate", reqEvent("voiceStateUpdate"));
+
   client.on("error", client.error);
   client.on("warn", client.warn);
 };
